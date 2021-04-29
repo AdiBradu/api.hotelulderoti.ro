ddd471088873d5106bbcfb5db9d0e1d27ce4800f
dan-alexandru8503 <dan.alexandru8503@gmail.com>
2021-04-29 14:30:43 +0300

    cors

--- src/server.js
+++ src/server.js
@@ -22,6 +22,7 @@ app.use(express.json());
 }
 app.use(cors(corsOptions));*/
 app.use(cors());
+app.options('*',cors());
 
 app.set('etag', false);
 
@@ -65,6 +66,11 @@ const port = Number(process.env.PORT || 3331);
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/fleets', fleetRouter);
+
+//testing purposes
+app.get('/test', (req, res) => {
+  res.send('online');
+})
 
 //404 error
 app.all('*', (req, res, next) => {
